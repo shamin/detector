c58c0d4b162d26faa1840340c295861c14df6923
shamin.meerankutty <shamin.meerankutty@fullcontact.com>
2018-10-16 00:29:55 +0530

    Fetching pull request details with graphql query

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
+const getPullRequest = `query getPullRequest($owner: String!, $name: String!, $number: Int!) {
+  repository(owner: $owner, name: $name) {
+    pullRequest(number: $number) {
+      id
+      number
+      title,
+      commits(last: 100)
+      {
+        nodes{
+          commit{
+            message
+          }
+        }
+      }
+    }
+  }
+}
+`
+
 module.exports = app => {
   app.log('Detecter app is loaded')
   app.on('pull_request', async context => {
-    app.log(context.payload)
+    const data = await context.github.query(getPullRequest, {owner: context.payload.repository.owner.login,name: context.payload.repository.name,number: context.payload.number})
+    await app.log(JSON.stringify(data))
   })
 }
