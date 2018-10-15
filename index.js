c9a146291cccc55d13ed83231092e0e7df0bb222
shamin.meerankutty <shamin.meerankutty@fullcontact.com>
2018-10-15 22:21:31 +0530

    Added webhook on pull request

--- index.js
+++ index.js
@@ -1,19 +1,6 @@
-/**
- * This is the entry point for your Probot App.
- * @param {import('probot').Application} app - Probot's Application class.
- */
 module.exports = app => {
-  // Your code here
-  app.log('Yay, the app was loaded!')
-
-  app.on('issues.opened', async context => {
-    const issueComment = context.issue({ body: 'Thanks for opening this issue!' })
-    return context.github.issues.createComment(issueComment)
+  app.log('Detecter app is loaded')
+  app.on('pull_request', async context => {
+    app.log(context.payload)
   })
-
-  // For more information on building apps:
-  // https://probot.github.io/docs/
-
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
 }
