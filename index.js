591f35699bca25e44957f583504445e73b52800e
shamin.meerankutty <shamin.meerankutty@fullcontact.com>
2018-10-16 19:46:55 +0530

    Getting data from file and pullrequest

--- index.js
+++ index.js
@@ -1,26 +1,13 @@
-const getPullRequest = `query getPullRequest($owner: String!, $name: String!, $number: Int!) {
-  repository(owner: $owner, name: $name) {
-    pullRequest(number: $number) {
-      id
-      number
-      title,
-      commits(last: 100)
-      {
-        nodes{
-          commit{
-            message
-          }
-        }
-      }
-    }
-  }
-}
-`
-
 module.exports = app => {
   app.log('Detecter app is loaded')
   app.on('pull_request', async context => {
-    const data = await context.github.query(getPullRequest, {owner: context.payload.repository.owner.login,name: context.payload.repository.name,number: context.payload.number})
-    await app.log(JSON.stringify(data))
+
+    try {
+      const data = await context.github.pullRequests.getFiles({ owner: context.payload.repository.owner.login, repo: context.payload.repository.name, number: context.payload.number })
+      const files = await context.github.repos.getContent({ owner: context.payload.repository.owner.login, repo: context.payload.repository.name,path: '.detector' })
+      await app.log(JSON.stringify(data))
+    } catch (e) {
+      app.log(e)
+    }
   })
 }
