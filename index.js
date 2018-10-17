4050cd00f2e18da2fe57ba5dcfe9362874f81d2d
shamin.meerankutty <shamin.meerankutty@fullcontact.com>
2018-10-17 20:01:37 +0530

    Added commenting to pullrequest on a file change

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
-const { pullRequest, file } = require('./test')
 const Detector = require('./lib/detector')
-
-Buffer = require('safer-buffer').Buffer
 
 module.exports = app => {
   app.log('Detecter app is loaded')
