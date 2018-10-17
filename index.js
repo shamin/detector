40d85c844f3e4782c586146085ab9bfa4eb3a682
shamin.meerankutty <shamin.meerankutty@fullcontact.com>
2018-10-17 18:14:34 +0530

    Add a detector class which gets the changed files

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
+const { pullRequest, file } = require('./test')
+const Detector = require('./lib/detector')
+
+Buffer = require('safer-buffer').Buffer
+
 module.exports = app => {
   app.log('Detecter app is loaded')
-  app.on('pull_request', async context => {
 
-    try {
-      const data = await context.github.pullRequests.getFiles({ owner: context.payload.repository.owner.login, repo: context.payload.repository.name, number: context.payload.number })
-      const files = await context.github.repos.getContent({ owner: context.payload.repository.owner.login, repo: context.payload.repository.name,path: '.detector' })
-      await app.log(JSON.stringify(data))
-    } catch (e) {
-      app.log(e)
-    }
+  app.on('pull_request', async context => {
+    const detector = new Detector(context)
+    detector.sendComments()
   })
 }
+
