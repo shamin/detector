cd83856297bac7efe5af3cae2705b0cfb037fa8c
shamin.meerankutty <shamin.meerankutty@fullcontact.com>
2018-10-25 02:30:53 +0530

    Added logging to pullrequest webhook

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ module.exports = app => {
   app.log('Detector app is loaded')
 
   app.on('pull_request', async context => {
+    app.log("Pull Request")
     const detector = new Detector(context)
     detector.sendComments()
   })
